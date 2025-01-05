9b041b1289bcaad02043c57af3f454b44e4b564f
Kibrewossen <47031138+kebtes@users.noreply.github.com>
2025-01-05 18:30:03 +0300

    Project wide settings

---
+++ Source/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mellow_Music_Player.Source
+{
+    internal static class Settings
+    {
+        public static string songsDirectory = "C:\\Users\\CompUser\\Desktop\\Mellow Music Player\\Sample";
+    }
+}
