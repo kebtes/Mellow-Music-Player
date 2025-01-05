e97e09d605de340a3fa73ce4e75b1511b2e2cf50
Kibrewossen <47031138+kebtes@users.noreply.github.com>
2025-01-05 18:28:45 +0300

    Added "Song" class

---
+++ Source/Models/Song.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mellow_Music_Player.Source.Models
+{
+    internal class Song
+    {
+        public string Title { get; set; }
+        public string[] Artists { get; set; }
+        public string Album { get; set; }
+        public string[] Genres { get; set; }
+        public TimeSpan Duration { get; set; }
+        public string FilePath { get; set; }
+        public Image AlbumArt { get; set; }
+    }
+}
