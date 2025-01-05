708cdf5245d0625a58a416881cda4758adb762bb
Kibrewossen <47031138+kebtes@users.noreply.github.com>
2025-01-05 18:30:59 +0300

    Initial Commit📦

---
+++ Source/Services/FileService.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mellow_Music_Player.Source;
+using Mellow_Music_Player.Source.Models;
+using System.Drawing;
+using TagLib;
+
+namespace Mellow_Music_Player.Source.Services
+{
+    internal class FileService
+    {
+        private string directory;
+        private string[] audioFiles;
+        private List<Song> songs;
+
+        public FileService() 
+        {
+            songs = new List<Song>();
+            directory = Settings.songsDirectory;
+            
+            try
+            {
+                audioFiles = Directory.GetFiles(directory, "*.mp3", SearchOption.TopDirectoryOnly);
+            } catch (FileNotFoundException e)
+            {
+                Debug.WriteLine("Error while trying to access path in FileService" + e);
+            }
+
+           
+            foreach (string file in audioFiles)
+            {
+                try
+                {
+                    Song song = new Song();
+
+                    // Create the TagLib file here to access the metadata
+                    using (TagLib.File tagFile = TagLib.File.Create(file))
+                    {
+                        song.Title = tagFile.Tag.Title ?? Path.GetFileNameWithoutExtension(file);
+                        song.Album = tagFile.Tag.Album ?? "";
+                        song.Artists = tagFile.Tag.AlbumArtists?.Length > 0 ? tagFile.Tag.AlbumArtists : new string[] { "Unknown Artist" };
+                        song.Genres = tagFile.Tag.Genres?.Length > 0 ? tagFile.Tag.Genres : new string[] { "Unknown Genre" };
+                        song.Duration = tagFile.Properties.Duration;
+
+                        if (tagFile.Tag.Pictures.Length > 0)
+                        {
+                            var bin = tagFile.Tag.Pictures[0].Data.Data;
+                            using (MemoryStream ms = new MemoryStream(bin))
+                            {
+                                song.AlbumArt = Image.FromStream(ms);
+                            }
+                        }
+
+                        song.FilePath = file;
+                        songs.Add(song);
+                    }
+
+                } 
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error processing file {file}: {ex.Message}");
+                    continue;
+                }
+        
+            }
+
+        }
+
+        public string[] getAudioFiles()
+        {
+            return audioFiles;
+        }
+
+        public List<Song> getSongs()
+        {
+            return songs;
+        }
+    }
+}
