c799198c402946bd5347fe94d86c87e225f9251c
Kibrewossen <47031138+kebtes@users.noreply.github.com>
2025-01-07 19:33:48 +0300

    Class to handle queue of songs

---
+++ Source/Models/SongQueue.cs
@@ -0,0 +1,51 @@
+﻿using Mellow_Music_Player.Source.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mellow_Music_Player.Source.Models
+{
+    public class SongQueue
+    {
+        private FileService fileService;
+        private List<Song> songs;
+        private static int currentIdx = -1;
+
+        public SongQueue()
+        {
+            fileService = new FileService();
+            songs = fileService.getSongs().ToList();
+        }
+        public Song GetCurrentSong()
+        {
+            if (currentIdx == -1 && songs.Any())
+            {
+                currentIdx = 0;
+            }
+
+            return currentIdx >= 0 ? songs[currentIdx] : null;
+        }
+
+        public Song GetNextSong()
+        {
+            if (!songs.Any()) return null;
+            currentIdx = (currentIdx + 1) % songs.Count;
+            return songs[currentIdx];
+        }
+
+        public Song GetPrevSong()
+        {
+            if (!songs.Any()) return null;
+            currentIdx = Math.Max(currentIdx - 1, 0);
+            return songs[currentIdx];
+        }
+
+        public List<Song> GetAllSongs()
+        {
+            return songs;
+        }
+        
+    }
+}
