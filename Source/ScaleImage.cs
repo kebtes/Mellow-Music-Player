09976574c4be87a80508ecb7fb54837e411540a7
Kibrewossen <47031138+kebtes@users.noreply.github.com>
2025-01-07 19:35:47 +0300

    Helper class to handle icon resizing

---
+++ Source/ScaleImage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mellow_Music_Player.Source
+{
+    public static class ScaleImage
+    {
+        public static Image scaleImage(Image originalImage, int width = 45, int height = 45)
+        {
+            Bitmap scaledBitmap = new Bitmap(width, height);
+
+            using (Graphics g = Graphics.FromImage(scaledBitmap))
+            {
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                g.DrawImage(originalImage, 0, 0, width, height);
+            }
+
+            return scaledBitmap;
+        }
+    }
+}
