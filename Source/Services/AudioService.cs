c505664f840b423a0f4c0008935731b8529980cd
Kibrewossen <47031138+kebtes@users.noreply.github.com>
2025-01-09 00:04:34 +0300

    Added the "musicPlayerPanel" instance to update UI databinding when new song is played

--- Source/Services/AudioService.cs
+++ Source/Services/AudioService.cs
@@ -1,15 +1,20 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
 using Mellow_Music_Player.Source.Models;
+using Mellow_Music_Player.UI.Forms;
 using NAudio.Wave;
 
 namespace Mellow_Music_Player.Source.Services
 {
-    internal class AudioService
+    public class AudioService
     {
         private IWavePlayer waveOut;
         private AudioFileReader audioFile;
         private SongQueue songQueue = new SongQueue();
         private Song currentSong;
+
+        private MusicPlayerPanel _musicPlayerPanelInstance;
 
         public void Play()
         {
@@ -50,15 +55,22 @@ namespace Mellow_Music_Player.Source.Services
                 currentSong = song;
 
                 waveOut = new WaveOutEvent();
-                audioFile = new AudioFileReader(song.FilePath);
+                audioFile = new AudioFileReader(currentSong.FilePath);
                 waveOut.Init(audioFile);
                 waveOut.Play();
+                _musicPlayerPanelInstance.setCurrentSong(currentSong);
 
             }
             catch(Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
+
+        }
+
+        public void SetMusicPlayerInstance(MusicPlayerPanel instance)
+        {
+            _musicPlayerPanelInstance = instance;
         }
     }
 }
