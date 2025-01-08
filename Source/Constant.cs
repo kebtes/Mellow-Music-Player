16522c94114089257a5b6e25ed53888dd72888a5
Kibrewossen <47031138+kebtes@users.noreply.github.com>
2025-01-09 00:31:12 +0300

    Service related constants

---
+++ Source/Constant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mellow_Music_Player.Source.Services
+{
+    public static class Constant
+    {
+        /*
+         Database connection string and sqlite .db file path
+         */
+        public static string DBFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets", "mellow.db");
+        public static string DBConnectionString = $"Data Source={DBFilePath};Version=3;";
+    }
+}
