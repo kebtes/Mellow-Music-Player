f08ea4e358d21ff6eed85d336786e4142f4e3985
Kibrewossen <47031138+kebtes@users.noreply.github.com>
2025-01-09 20:59:52 +0300

    Code cleanup

--- Source/Settings.cs
+++ Source/Settings.cs
@@ -6,8 +6,29 @@ using System.Threading.Tasks;
 
 namespace Mellow_Music_Player.Source
 {
-    internal static class Settings
+    public class Settings
     {
-        public static string songsDirectory = "C:\\Users\\CompUser\\Desktop\\Mellow Music Player\\Sample";
+        private static string songsDirectory = "C:\\Users\\CompUser\\Desktop\\Mellow Music Player\\Sample";
+
+        private float volume = 1.0f;
+
+        public float Volume
+        {
+            get => volume;
+            set
+            {
+                if (value >= 0.0f && value <= 1.0f)
+                {
+                    volume = value;
+                }
+            }
+        }
+
+        public static string SongsDirectory
+        {
+            get => songsDirectory;
+        }
+
+        public Settings() { }
     }
 }
