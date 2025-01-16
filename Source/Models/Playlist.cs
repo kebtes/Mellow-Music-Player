ed193d6120ad1a0bbc090fe8f300226370379bd4
Kibrewossen <47031138+kebtes@users.noreply.github.com>
2025-01-16 12:37:40 +0300

    Playlist

---
+++ Source/Models/Playlist.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mellow_Music_Player.Source.Models
+{
+    public class Playlist
+    {
+        private int playlistId;
+        private string playlistName;
+        private DateTime createdAt;
+        private string playlistColor;
+
+        public int PlaylistId
+        {
+            get => playlistId;
+            set => playlistId = value;
+        }
+
+        public string PlaylistName
+        {
+            get => playlistName;
+            set => playlistName = value;
+        }
+
+        public string PlaylistColor
+        {
+            get => playlistColor; 
+            set => playlistColor = value;
+        }
+
+        public DateTime CreatedAt
+        {
+            get => createdAt;
+            set => createdAt = value;
+        }
+    }
+}
