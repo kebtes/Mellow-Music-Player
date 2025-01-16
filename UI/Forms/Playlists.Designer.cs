9963995e654b8b64c757ad22aa306ed71f023a59
Kibrewossen <47031138+kebtes@users.noreply.github.com>
2025-01-16 12:37:30 +0300

    Playlist page

---
+++ UI/Forms/Playlists.Designer.cs
@@ -0,0 +1,126 @@
+﻿using System.Windows.Forms;
+
+namespace Mellow_Music_Player.UI.Forms
+{
+    partial class Playlists
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label label1;
+            this.separator = new System.Windows.Forms.Panel();
+            this.playlistName = new System.Windows.Forms.Label();
+            this.verticalSeparator = new System.Windows.Forms.Panel();
+            this.songLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.playlistLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            label1 = new System.Windows.Forms.Label();
+            this.songLayoutPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // separator
+            // 
+            this.separator.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(27)))), ((int)(((byte)(34)))));
+            this.separator.Location = new System.Drawing.Point(4, 142);
+            this.separator.Name = "separator";
+            this.separator.Size = new System.Drawing.Size(950, 2);
+            this.separator.TabIndex = 2;
+            // 
+            // playlistName
+            // 
+            this.playlistName.AutoSize = true;
+            this.playlistName.Font = new System.Drawing.Font("Geist", 18F, System.Drawing.FontStyle.Bold);
+            this.playlistName.ForeColor = System.Drawing.Color.White;
+            this.playlistName.Location = new System.Drawing.Point(12, 101);
+            this.playlistName.Name = "playlistName";
+            this.playlistName.Size = new System.Drawing.Size(196, 38);
+            this.playlistName.TabIndex = 1;
+            this.playlistName.Text = "Liked Songs";
+            // 
+            // verticalSeparator
+            // 
+            this.verticalSeparator.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(27)))), ((int)(((byte)(34)))));
+            this.verticalSeparator.Location = new System.Drawing.Point(587, 142);
+            this.verticalSeparator.Name = "verticalSeparator";
+            this.verticalSeparator.Size = new System.Drawing.Size(2, 540);
+            this.verticalSeparator.TabIndex = 3;
+            // 
+            // songLayoutPanel
+            // 
+            this.songLayoutPanel.Controls.Add(label1);
+            this.songLayoutPanel.Location = new System.Drawing.Point(4, 150);
+            this.songLayoutPanel.Name = "songLayoutPanel";
+            this.songLayoutPanel.Size = new System.Drawing.Size(577, 532);
+            this.songLayoutPanel.TabIndex = 4;
+            // 
+            // playlistLayoutPanel
+            // 
+            this.playlistLayoutPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.playlistLayoutPanel.Location = new System.Drawing.Point(595, 150);
+            this.playlistLayoutPanel.Name = "playlistLayoutPanel";
+            this.playlistLayoutPanel.Size = new System.Drawing.Size(269, 532);
+            this.playlistLayoutPanel.TabIndex = 5;
+            this.playlistLayoutPanel.WrapContents = false;
+            // 
+            // label1
+            // 
+            label1.Font = new System.Drawing.Font("Geist", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(27)))), ((int)(((byte)(34)))));
+            label1.Location = new System.Drawing.Point(3, 0);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(574, 532);
+            label1.TabIndex = 0;
+            label1.Text = "Choose a Playlist to show!!";
+            label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Playlists
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(14)))), ((int)(((byte)(21)))));
+            this.ClientSize = new System.Drawing.Size(876, 694);
+            this.Controls.Add(this.playlistLayoutPanel);
+            this.Controls.Add(this.songLayoutPanel);
+            this.Controls.Add(this.verticalSeparator);
+            this.Controls.Add(this.playlistName);
+            this.Controls.Add(this.separator);
+            this.DoubleBuffered = true;
+            this.Name = "Playlists";
+            this.Text = "Playlists";
+            this.songLayoutPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel separator;
+        private System.Windows.Forms.Label playlistName;
+        private System.Windows.Forms.Panel verticalSeparator;
+        private System.Windows.Forms.FlowLayoutPanel songLayoutPanel;
+        private System.Windows.Forms.FlowLayoutPanel playlistLayoutPanel;
+    }
+}
---
+++ UI/Forms/Playlists.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Drawing;
+
+using Mellow_Music_Player.Source.Models;
+using Mellow_Music_Player.Source.Services.Database_Services;
+using Mellow_Music_Player.Source.Services;
+using Mellow_Music_Player.Source;
+using Mellow_Music_Player.Source.Services.DatabaseServices;
+
+
+namespace Mellow_Music_Player.UI.Forms
+{
+    public partial class Playlists : Form
+    {
+        AudioService audioService;
+        MusicPlayerPanel musicPlayerPanel;
+
+        public Playlists(AudioService audioService)
+        {
+            this.audioService = audioService;
+            //this.musicPlayerPanel = musicPlayerPanel;
+
+            string lastPlayedPlaylist = Settings.LastOpenedPlaylist;
+
+            if (lastPlayedPlaylist != null) { 
+            }
+            InitializeComponent();
+            LoadPlaylists();
+        }
+
+        public void LoadPlaylists()
+        {
+            //MessageBox.Show(playlistLayoutPanel.Visible.ToString());
+            List<Playlist> playlists = DatabaseService.GetPlaylists();
+
+            foreach (var playlist in playlists)
+            {
+                CreatePlaylistLabels(playlist, playlistLayoutPanel);
+            }
+        }
+
+        private Label selectedPlaylistLabel = null;
+        private void CreatePlaylistLabels(Playlist playlist, Panel parentPanel)
+        {
+            const string unselColor = "#E0DCDC";
+
+            Label playlistNameLabel = new Label
+            {
+                Text = playlist.PlaylistName,
+                Font = new Font(Constants.ProjectFont, 8, FontStyle.Bold),
+                ForeColor = ColorTranslator.FromHtml(unselColor),
+                BackColor = Color.Transparent,
+                Cursor = Cursors.Hand,
+            };
+
+            playlistNameLabel.MouseEnter += (sender, e) =>
+            {
+                if (playlistNameLabel != selectedPlaylistLabel)
+                {
+                    playlistNameLabel.Font = new Font(Constants.ProjectFont, 8, FontStyle.Bold | FontStyle.Underline);
+                }
+            };
+
+            playlistNameLabel.MouseLeave += (sender, e) =>
+            {
+                if (playlistNameLabel != selectedPlaylistLabel)
+                {
+                    playlistNameLabel.Font = new Font(Constants.ProjectFont, 8, FontStyle.Bold);
+                }
+            };
+
+            playlistNameLabel.Click += (sender, e) =>
+            {
+                if (selectedPlaylistLabel != null)
+                {
+                    selectedPlaylistLabel.Font = new Font(Constants.ProjectFont, 8, FontStyle.Bold);
+                    selectedPlaylistLabel.ForeColor = ColorTranslator.FromHtml(unselColor);
+                }
+
+                selectedPlaylistLabel = playlistNameLabel;
+                selectedPlaylistLabel.Font = new Font(Constants.ProjectFont, 8, FontStyle.Bold | FontStyle.Underline);
+                selectedPlaylistLabel.ForeColor = ColorTranslator.FromHtml(Constants.OrangeColor);
+
+                LoadPlaylistSongs(playlist);
+            };
+
+            parentPanel.Controls.Add(playlistNameLabel);
+        }
+
+        private void LoadPlaylistSongs(Playlist playlist)
+        {
+            songLayoutPanel.Controls.Clear();
+            List<Song> songs = DatabaseService.GetPlaylistSongs(playlist.PlaylistName);
+            
+            if (songs.Count == 0)
+            {
+                PlaylistEmpty();
+            }
+            else
+            {
+                foreach (var song in songs)
+                {
+                    CreateSongCard(song, songLayoutPanel);
+                }
+            }
+        }
+
+        private void PlaylistEmpty()
+        {
+            songLayoutPanel.Controls.Clear();
+            Label playlistEmptyLabel = new Label
+            {
+                Text = "No songs added to this playlist :(",
+                Font = new Font(Constants.ProjectFont, 10, FontStyle.Bold),
+                ForeColor = ColorTranslator.FromHtml("#181B22"),
+                Size = new Size(574, 500),
+                Location = new Point(0, 0),
+                TextAlign = ContentAlignment.MiddleCenter,
+                AutoSize = false,
+            };
+
+            songLayoutPanel.Controls.Add(playlistEmptyLabel);
+        }
+
+        private Panel currentlySelectedPanel = null;
+        public void CreateSongCard(Song s, Panel parentComponent, Fields? fieldHighlight = null)
+        {
+            const int cardWidth = 723;
+            const int cardHeight = 61;
+
+            bool isHovered = false;
+
+            // Panel to hold the song
+            Panel panelSongCard = new Panel
+            {
+                Size = new Size(cardWidth, cardHeight),
+                BackColor = ColorTranslator.FromHtml(Constants.DarkBlue),
+                BorderStyle = BorderStyle.None,
+                ForeColor = Color.White
+            };
+
+            Label lblSongTitle = new Label
+            {
+                Text = s.Title,
+                Size = new Size(193, 13),
+                Font = new Font(Constants.ProjectFont, 7, FontStyle.Bold),
+                ForeColor = Color.White,
+                Location = new Point(74, 20)
+            };
+
+            Label lblArtist = new Label()
+            {
+                Text = string.Join(", ", s.Artists),
+                Size = new Size(190, 10),
+                Font = new Font(Constants.ProjectFont, 5, FontStyle.Regular),
+                ForeColor = Color.White,
+                Location = new Point(74, 36)
+            };
+
+            Panel panelAlbumArt = new Panel
+            {
+                Size = new Size(45, 45),
+                Location = new Point(20, 8),
+                BackgroundImage = ScaleImage.Scale(s.AlbumArt)
+            };
+
+            Button playButton = new Button
+            {
+                Size = new Size(30, 30),
+                BackgroundImage = ScaleImage.Scale(Image.FromFile(Constants.PlayButtonSmallIcon), 30, 30),
+                Location = new Point(500, 15),
+            };
+
+            playButton.MouseClick += (sender, e) =>
+            {
+                // Deselect previously selected panel
+                if (currentlySelectedPanel != null)
+                {
+                    currentlySelectedPanel.BackColor = ColorTranslator.FromHtml(Constants.DarkBlue);
+                    currentlySelectedPanel.Invalidate(true);
+
+                }
+
+                currentlySelectedPanel = panelSongCard;
+                panelSongCard.BackColor = ColorTranslator.FromHtml(Constants.HoverBlue);
+                panelSongCard.Invalidate();
+
+                //musicPlayerPanel.ClearProgress();
+                //musicPlayerPanel.SetPlaying(true);
+                audioService.Play(s);
+            };
+
+            playButton.MouseEnter += (sender, e) =>
+            {
+                playButton.BackgroundImage = ScaleImage.Scale(Image.FromFile(Constants.PlayButtonSmallSelectedIcon), 30, 30);
+            };
+
+            playButton.MouseLeave += (sender, e) =>
+            {
+                playButton.BackgroundImage = ScaleImage.Scale(Image.FromFile(Constants.PlayButtonSmallIcon), 30, 30);
+            };
+
+            playButton.FlatStyle = FlatStyle.Flat;
+            playButton.FlatAppearance.BorderSize = 0;
+
+            panelSongCard.MouseEnter += (sender, e) =>
+            {
+                if (panelSongCard == currentlySelectedPanel) return;
+
+                isHovered = true;
+                panelSongCard.Invalidate();
+            };
+
+            panelSongCard.MouseLeave += (sender, e) =>
+            {
+                if (panelSongCard == currentlySelectedPanel) return;
+
+                isHovered = false;
+                panelSongCard.Invalidate();
+            };
+
+            panelSongCard.MouseClick += (sender, e) =>
+            {
+                // Deselect previously selected panel
+                if (currentlySelectedPanel != null)
+                {
+                    currentlySelectedPanel.BackColor = ColorTranslator.FromHtml(Constants.DarkBlue);
+                    currentlySelectedPanel.Invalidate(true);
+
+                }
+
+                currentlySelectedPanel = panelSongCard;
+                panelSongCard.BackColor = ColorTranslator.FromHtml(Constants.HoverBlue);
+                panelSongCard.Invalidate();
+            };
+
+            panelSongCard.Paint += (sender, e) =>
+            {
+                if (panelSongCard == currentlySelectedPanel)
+                {
+                    panelSongCard.BackColor = ColorTranslator.FromHtml(Constants.HoverBlue);
+
+                    using (Pen pen = new Pen(ColorTranslator.FromHtml(Constants.OrangeColor), Constants.HoverPenWidth))
+                    {
+                        e.Graphics.DrawLine(pen, 0, 0, 0, panelSongCard.Height);
+                    }
+                }
+                else if (isHovered)
+                {
+                    //using (Pen pen = new Pen(ColorTranslator.FromHtml(Constants.HoverGrey), Constants.HoverPenWidth))
+                    //{
+                    //    e.Graphics.DrawLine(pen, 0, 0, 0, panelSongCard.Height);
+                    //}
+
+                    panelSongCard.BackColor = ColorTranslator.FromHtml(Constants.HoverGrey);
+                }
+                else
+                {
+                    panelSongCard.BackColor = ColorTranslator.FromHtml(Constants.DarkBlue);
+                }
+            };
+
+            panelSongCard.Controls.Add(lblSongTitle);
+            panelSongCard.Controls.Add(lblArtist);
+            panelSongCard.Controls.Add(panelAlbumArt);
+            panelSongCard.Controls.Add(playButton);
+            //panelSongCard.Controls.Add(deleteButton);
+
+            parentComponent.Controls.Add(panelSongCard);
+
+        }
+
+    }
+}
