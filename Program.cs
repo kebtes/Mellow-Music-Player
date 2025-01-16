b5b539a541948e3b0033d7603deb29ab35f1e905
Kibrewossen <47031138+kebtes@users.noreply.github.com>
2025-01-16 17:57:25 +0300

    Code cleanup

--- Program.cs
+++ Program.cs
@@ -11,12 +11,6 @@ namespace Mellow_Music_Player
 {
     internal static class Program
     {
-        public async static void lyrics()
-        {
-            var lyrics = await LyricsService.GetLyrics("The Nights", "Avicii");
-            Console.Write(lyrics.ToString());
-            lyrics.ForEach(line => Console.WriteLine(line));
-        }
         public static Settings settings;
         /// <summary>
         /// The main entry point for the application.
@@ -30,27 +24,11 @@ namespace Mellow_Music_Player
             DatabaseService.InitializeDatabase();
             FileService.Refresh();
 
-            lyrics();
-            //DatabaseService.AddToPlaylist("Liked Songs", 1);
             settings = new Settings();
-            
-            
+               
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Main(settings));
-
-            //Console.WriteLine(DatabaseService.GetPlaylists().ToArray()[0].PlaylistName);
-            //List<Song> s = DatabaseService.GetPlaylistSongs("Liked Songs");
-            //Console.WriteLine(s.Count);
-            //Console.WriteLine(DatabaseService.DebugPlaylistSongs());
-            //DatabaseService.DebugPlaylistSongs();
-
-
-
-
-
-
-
         }
 
         [System.Runtime.InteropServices.DllImport("user32.dll")]
