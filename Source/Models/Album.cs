dc3a88870be532ace9b8e136d658c5bac93ba212
Kibrewossen <47031138+kebtes@users.noreply.github.com>
2025-01-17 18:41:58 +0300

    Add Album class.

---
+++ Source/Models/Album.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace Mellow_Music_Player.Source.Models
+{
+    public class Album
+    {
+        public string AlbumName { get; set; }
+        public string Artists { get; set; }
+        public Image AlbumArt { get; set; }
+        public List<Song> Songs { get; set; }
+    }
+}
