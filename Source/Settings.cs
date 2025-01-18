55ed92a86a16d7389a94e05bddb8a880909a94ee
Kibrewossen <47031138+kebtes@users.noreply.github.com>
2025-01-18 20:01:00 +0300

    Refactor Settings class and simplify property setters

--- Source/Settings.cs
+++ Source/Settings.cs
@@ -1,11 +1,14 @@
 ﻿namespace Mellow_Music_Player.Source
 {
-    public static class Settings
+    public class Settings
     {
-        //private static string songsDirectory = "C:\\Users\\CompUser\\Mellow-Music-Player\\Sample";
         private static string songsDirectory;
         private static float volume = 1.0f;
         private static string lastOpenedPlaylist = null;
+        private static string filePath = "settings.json";
+
+        private Settings() { }
+
         public static float Volume
         {
             get => volume;
@@ -20,14 +23,14 @@
 
         public static string SongsDirectory
         {
-            set { songsDirectory = value; }
             get => songsDirectory;
+            set => songsDirectory = value;
         }
 
         public static string LastOpenedPlaylist
         {
             get => lastOpenedPlaylist;
-            set { lastOpenedPlaylist = value; }
+            set => lastOpenedPlaylist = value;
         }
     }
 }
